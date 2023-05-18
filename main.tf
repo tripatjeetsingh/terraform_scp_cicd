73a2981ed56bd82b87135ff8ae68347298e4966c
Tripatjeet Singh <Tripatjeet.Singh@umb.com>
2023-05-18 16:48:19 -0500

    bucket name update

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "new_bucket" {
-  bucket = "demo-github-action-tf-04203023"
+  bucket = "demo-github-action-tf-04203023_${timestamp()}"
 
   object_lock_enabled = false
 
