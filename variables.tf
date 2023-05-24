485eeafe7c15736970407ae61b65ca1e35fa413d
Tripatjeet Singh <Tripatjeet.Singh@umb.com>
2023-05-24 09:22:27 -0500

    Added Variables

---
+++ variables.tf
@@ -0,0 +1,13 @@
+#-----security_controls_scp/variables.tf----#
+variable "target_id" {
+  description = "The Root ID, Organizational Unit ID, or AWS Account ID to apply SCPs."
+  type        = string
+}
+
+variable "region_lockdown" {
+  description = "The AWS region(s) you want to restrict resources to."
+  type        = list(string)
+  default = [
+    "us-east-2",
+  ]
+}
