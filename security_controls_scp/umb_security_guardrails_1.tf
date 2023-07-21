7a3311d5e1d4e13230a05b37fb26eb6f38a83dec
Tripatjeet Singh <Tripatjeet.Singh@umb.com>
2023-07-21 17:22:50 -0500

    updated the baseline 1

--- security_controls_scp/umb_security_guardrails_1.tf
+++ security_controls_scp/umb_security_guardrails_1.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "umb_security_guardrails_1" {
     }
   }
   statement {
-    sid       = "Denyattachuserpolicy"
+    sid       = "DenyAttachUserPolicy"
     effect    = "Deny"
     actions   = ["iam:AttachUserPolicy", ]
     resources = ["*", ]
@@ -73,7 +73,7 @@ data "aws_iam_policy_document" "umb_security_guardrails_1" {
     }
   }
   statement {
-    sid       = "RequireEC2snapshotencryption"
+    sid       = "RequireEC2snapshotEncryption"
     actions   = ["ec2:ImportSnapshot", "ec2:CreateSnapshot", "ec2:RestoreSnapshotFromRecycleBin", "ec2:RestoreSnapshotTier"]
     resources = ["arn:aws:ec2:*:*:snapshot/*", ]
     effect    = "Deny"
