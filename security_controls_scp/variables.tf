e10bd8848137ae11940622584c5cabf9988f5347
Tripatjeet Singh <Tripatjeet.Singh@umb.com>
2023-07-21 10:59:14 -0500

    Added  variable definition for imdsv2_max_hop

--- security_controls_scp/variables.tf
+++ security_controls_scp/variables.tf
@@ -8,3 +8,9 @@ variable "region_lockdown" {
   description = "The AWS region(s) you want to restrict resources to."
   type        = list(string)
 }
+
+variable "imdsv2_max_hop" {
+  description = "The maximum hop allowed for an IMDSv2 token."
+  default     = 1
+  type        = number
+}
